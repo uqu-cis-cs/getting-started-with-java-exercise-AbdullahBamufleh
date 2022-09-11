673575d1bacb63ab863c444c63e51c42c8b16db7
AbdullahBamufleh <s443010650s443010650@st.uqu.edu.sa>
2022-09-11 11:36:53 +0300

    my first git commit

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -6,15 +6,14 @@ package edu.uqu.cs;
  * 1. Rename the class name from FirstExercise to App
  * 2. Add the public keyword before the class keyword
  * 3. Fix the main mehtod declaration
- * 4. Before submiting your code, modify the text “Hello World” to “Hello: <Name>, <Student ID>” in App.java
- *      a.	Change <Name> to your name
- *      b.	Change <Student ID> to your student ID
+ * 4. Before submiting your code, modify the text “Hello World” to “Hello:
+ * <Name>, <Student ID>” in App.java
+ * a. Change <Name> to your name
+ * b. Change <Student ID> to your student ID
  */
- 
-class FirstExercise
-{
-    private static void mainMethod( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Abdullah, 443010650");
     }
 }
